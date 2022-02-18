f9e2496a45b6a60235dd1588336fc96ce41d12b1
JaeCheon <studyJcHub@gmail.com>
2022-02-18 11:12:13 +0900

    Handler 를 이용해서, 2.5초 후에 메인화면으로 이동

--- app/src/main/java/com/jc/androidappsetting_20220218/SplashActivity.kt
+++ app/src/main/java/com/jc/androidappsetting_20220218/SplashActivity.kt
@@ -1,11 +1,28 @@
 package com.jc.androidappsetting_20220218
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+//        화면 이동 ~ 로딩화면 종료를, 2.5초 후에 실행시키고 싶다.
+//        UI 동작을 담당하는 Thread 에게, 2.5초 후에 이동하는 코드를 실행시켜 달라고 등록.
+//        등록 해주는 클래스 - Handler
+        val myHandler = Handler(Looper.getMainLooper()) // Main Thread 와 통신하는 Handler 생성
+        myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+//        화면을 이동하고 나면, 로딩화면을 유지할 필요가 없다.
+            finish()
+        }, 2500)
+
+
     }
 }
