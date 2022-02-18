ab5f933273d3acac39f7ca0fb3961df9824be5e3
JaeCheon <studyJcHub@gmail.com>
2022-02-18 10:12:15 +0900

    SplashActivity 화면 추가  - manifests 에 태그 하나 추가 됨

---
+++ app/src/main/java/com/jc/androidappsetting_20220218/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.jc.androidappsetting_20220218
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
